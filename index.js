e5a7e431cbe5bda998430a0d372efc3882afdbc0
Mohammad-mahdi2040 <mohamad.mahdi2040@gmail.com>
2021-06-14 13:48:24 +0430

    main

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('mohammad fake Online Shod!');
+});
+
+client.on("ready", () => {
+    function AliveVoice() {
+        const targetguild = client.guilds.cache.get("716511276242829324")
+        const voiceChannels = targetguild.channels.cache.filter(c => c.type === 'voice');
+        let count = 0;
+
+        for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
+        client.user.setActivity(`🔊 Total Mic | ${count}`, { type: "LISTENING" })
+    }; setInterval(AliveVoice, 3000)
+});
+
+client.login('ODUzOTIyMTc4MDUwMjkzNzYw.YMcbAA.NKFlVCsuFXh4WK90bsY-AIxb2EA');
