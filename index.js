b4714110ddd79446bc9b3482dd32c049c14c9f69
Mohammad Mehdi <84872648+Mohammad-mahdi2040@users.noreply.github.com>
2021-08-01 11:08:37 +0430

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('your-simlatour-bot-now-online!');
+	console.log('Bot Now Ready!');
 });
 
 client.login('you-bot-token');
